ad3c3a4c1321eceb60526a20fc364d7c1946a911
Aditya <aditya@INFLAP027.local>
2018-10-30 17:10:55 +0530

    Initial commit

---
+++ InfrrdOCR/InfrrdOCR.h
@@ -0,0 +1,19 @@
+//
+//  InfrrdOCR.h
+//  InfrrdOCR
+//
+//  Created by Aditya on 10/30/18.
+//  Copyright © 2018 Aditya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfrrdOCR.
+FOUNDATION_EXPORT double InfrrdOCRVersionNumber;
+
+//! Project version string for InfrrdOCR.
+FOUNDATION_EXPORT const unsigned char InfrrdOCRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfrrdOCR/PublicHeader.h>
+
+
